23d8ff97395112f8ff92d8a8e8c929edf5020a16
luorenshu <626115221@qq.com>
2022-01-13 14:51:52 +0800

    这个版本可以编译通过了

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export JAVA_HOME="C:\Program Files\Java\jdk1.8.0_301"
+export PATH="$JAVA_HOME\bin;$PATH"
+#export GRADLE_HOME="D:\software\gradle-5.4"
+export GRADLE_HOME="D:\software\gradle-5.2.1"
+export PATH="$GRADLE_HOME\bin;$PATH"
+
+/d/software/gradle-5.2.1/bin/gradle clean build
